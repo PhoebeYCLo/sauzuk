33fda077cc58212bc31a5e998cd67854c08c9345
Phoebe Lo <phoebeyclo@gmail.com>
2020-10-23 00:24:14 -0700

    axios get protest from database success

--- client/src/components/Protests/ProtestCard.jsx
+++ client/src/components/Protests/ProtestCard.jsx
@@ -1,28 +1,20 @@
-import React, { Component } from 'react';
-import hkprotest from '../../assets/images/hkprotest.jpeg';
+import React from 'react';
 
-class ProtestCard extends Component {
-    render() {
-        return (
-            <div className="protest__list-container">
-                <div className="protest__card-container">
-                    <div className="protest__card-image-container">
-                        <img src={hkprotest} alt="protest image" />
-                    </div>
-                    <div className="protest__card-info-container">
-                        <div className="protest__card-info-date">Sunday, 9 June 2020</div>
-                        <div className="protest__card-info-title">Hong Kong anti-extradition bill protest</div>
-                        <div className="protest__card-info-description">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-                        Pellentesque id nibh tortor id aliquet lectus. Felis imperdiet proin fermentum leo vel orci porta. In eu mi bibendum neque 
-                        egestas congue quisque. Morbi tempus iaculis urna id volutpat lacus. Eu nisl nunc mi ipsum faucibus vitae aliquet nec. Curabitur 
-                        vitae nunc sed velit dignissim sodales ut. Nec nam aliquam sem et tortor consequat id. 
-                        </div>
-                    </div>
+const ProtestCard = (props) => {
+    return (
+        <div className="protest__card-container">
+            <div className="protest__card-image-container">
+                <img src={props.image} alt="protest image" />
+            </div>
+            <div className="protest__card-info-container">
+                <div className="protest__card-info-date">{props.date}</div>
+                <div className="protest__card-info-title">{props.name}</div>
+                <div className="protest__card-info-description">
+                {props.description}
                 </div>
             </div>
-        )
-    }
+        </div>
+    )
 }
 
 export default ProtestCard;
--- client/src/components/Protests/Protests.jsx
+++ client/src/components/Protests/Protests.jsx
@@ -6,18 +6,15 @@ import axios from 'axios';
 const URL = "http://localhost:8080";
 
 class Protests extends Component {
-    constructor(props){
-        super();
-        this.state = {
-            protests: []
-        }
+    state = {
+        protests: []
     }
 
     getProtest() {
         axios.get(`${URL}/`)
             .then(res =>
-                // console.log(res.data))
-                this.setState({ protests: res.data }))
+                // console.log(res.data.protests))
+                this.setState({ protests: res.data.protests}))
             .catch(err => console.log(err));
     }
 
@@ -34,7 +31,16 @@ class Protests extends Component {
                             placeholder="Search ..."
                             className="protest__search-input" />
                     </div>
-                    <ProtestCard />
+                    <div className="protest__list-container">
+                        {this.state.protests.map(data => 
+                            <ProtestCard 
+                                image={data.image}
+                                name={data.name}
+                                date={data.date}
+                                description={data.description}
+                            />
+                        )}
+                    </div>
                 </div>
             </div>
         )
